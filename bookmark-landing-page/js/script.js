7c43dcae017ebd327926cd2256e66aa29afd6fea
SensiPhi <lailtoneletrohits@hotmail.com>
2020-06-09 21:14:04 -0300

    Tab navigation script

--- bookmark-landing-page/js/script.js
+++ bookmark-landing-page/js/script.js
@@ -4,3 +4,33 @@ const menuHamburger = document.querySelector(".menu__hamburger");
 const menuUl = document.querySelector(".menu__ul");
 
 menuHamburger.addEventListener("click", () => menuUl.classList.toggle("open"));
+
+/* Tab navigation */
+
+const featuresLi = document.querySelectorAll(".features__nav li");
+const features = document.querySelectorAll(".features__feature");
+
+function tabNavigation(index) {
+  if (!event.target.classList.contains("active")) {
+    featuresLi.forEach((li) => {
+      li.classList.remove("active");
+    });
+    event.target.classList.add("active");
+
+    features.forEach((feature) => {
+      const dataFeature = feature.getAttribute("data-feature");
+      if (dataFeature == index) {
+        features.forEach((item) => {
+          item.classList.remove("active");
+          setTimeout(() => (item.style.display = "none"), 300);
+        });
+        feature.classList.add("active");
+        setTimeout(() => (feature.style.display = "flex"), 300);
+      }
+    });
+  }
+}
+
+featuresLi.forEach((li, index) => {
+  li.addEventListener("click", () => tabNavigation(index));
+});
